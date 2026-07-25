2fb094ea2b6dc42df19bb814edb384ee5d9e7a00
Pratyush1110 <ranjan.pratyush24@gmail.com>
2026-07-26 00:38:54 +0530

    initial commit

---
+++ src/app/documents/[documentId]/ruler.tsx
@@ -0,0 +1,14 @@
+const markers = Array.from({ length: 83 })
+
+export const Ruler = () => {
+    return (
+        <div className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
+            <div
+                id="ruler-container"
+                className="max-w-[816px] mx-auto w-full h-full relative"
+            >
+
+            </div>
+        </div>
+    );
+}
