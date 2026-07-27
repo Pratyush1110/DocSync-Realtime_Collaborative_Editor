b94a53cd9e3af44526989ba6741959a80b115394
Pratyush1110 <ranjan.pratyush24@gmail.com>
2026-07-27 09:17:14 +0530

    ruler fix + Navigation bar tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -85,22 +85,6 @@ export const Editor = () => {
             TableCell,
             TableHeader,
         ],
-        content: `
-        <table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th colspan="3">Description</th>
-            </tr>
-            <tr>
-              <td>Cyndi Lauper</td>
-              <td>Singer</td>
-              <td>Songwriter</td>
-              <td>Actress</td>
-            </tr>
-          </tbody>
-        </table>
-      `,
         editorProps: {
             attributes: {
                 style: "padding-left: 56px; padding-right: 56px; ",
